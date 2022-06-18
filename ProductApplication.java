c47c6c55139f57013a58f587b14a514da9f4d1ef
yan-ateneu <83308503+yan-ateneu@users.noreply.github.com>
2022-06-19 01:23:34 +0300

    Add files via upload

---
+++ ProductApplication.java
@@ -0,0 +1,15 @@
+package com.proiect2.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+
+@SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
