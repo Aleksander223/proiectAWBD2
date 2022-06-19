ba28e0451e6a8dc373c982092ca8e282a0744fa2
yan-ateneu <83308503+yan-ateneu@users.noreply.github.com>
2022-06-19 18:25:40 +0300

    Delete repositories directory

--- repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proiect2.product.repositories;
-
-import com.proiect2.product.model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    Product findByCategoryAndBrand(String category, String brand);
-}
