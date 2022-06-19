23657601846a32669612140bb7d62cef3b1c3f47
yan-ateneu <83308503+yan-ateneu@users.noreply.github.com>
2022-06-19 16:54:34 +0300

    Add files via upload

---
+++ model/Product.java
@@ -0,0 +1,39 @@
+package com.proiect2.product.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "product")
+public class Product extends RepresentationModel<Product>{
+@Id
+@GeneratedValue(strategy= GenerationType.IDENTITY)
+@Column(name = "id")
+private Long id;
+
+@Column(name="name")
+@Size(max=20, message = "max 20 ch.")
+private String name;
+
+@Column(name="category")
+@Size(max=30, message = "max 30 ch.")
+private String category;
+
+@Column(name="brand")
+@Size(max=30, message = "max 30 ch.")
+private String brand;
+
+@Column(name="description")
+@Size(max=300, message = "max 300 ch.")
+private String description;
+
+@Column(name="price")
+private int price;
+}
